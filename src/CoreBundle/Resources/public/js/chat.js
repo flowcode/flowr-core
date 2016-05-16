36b96b2a07f445e7d20d908d4ed8158e0e8018c9
Juan Manuel Aguero <juanma.aguero@gmail>
2016-05-15 21:05:09 -0300

    Chat testing

---
+++ src/CoreBundle/Resources/public/js/chat.js
@@ -0,0 +1,32 @@
+/**
+ * Created by juanma on 5/15/16.
+ */
+
+socket.on('connect', function () {
+    socket.emit('adduser', username, '#general');
+});
+
+$('form').submit(function () {
+    socket.emit('sendchat', {'user': username, 'msg': $('#m').val()});
+    $('#m').val('');
+    return false;
+});
+
+function appendMessage(data) {
+    var msg = '<b>' + data.user + '</b>: ' + data.msg;
+    $('#messages').append('<li>' + msg + '</li>');
+    $('#messages').animate({scrollTop: $('#messages').height()});
+}
+
+socket.on('updatechat', function (data) {
+    appendMessage(data)
+});
+
+socket.on('p-' + username, function (data) {
+    data.user = '<img src="'+ flowr_logo +'" width="15">';
+    appendMessage(data)
+});
+
+$('#chat-toggle').click(function () {
+    $('#chat').toggleClass('collapsed');
+});
